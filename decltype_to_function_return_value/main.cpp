46264146860612c31ea0d111baee8d0192b244c1
Roman Peresipkyn <r.peresipkyn@samsung.com>
2016-12-19 11:17:33 +0200

    decltype to function return value

---
+++ decltype_to_function_return_value/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <type_traits>
+
+using namespace std;
+
+int foo() { return 42; }
+string bar() { return "hello"; }
+
+template <typename FunctionType>
+void ex1(FunctionType f) {
+	typedef typename result_of<FunctionType()>::type result_type;
+	result_type a = f();
+	cout << a << endl;
+}
+
+int main()
+{
+	ex1<int(*)()>(foo);
+	ex1<string(*)()>(bar);
+
+	return 0;
+}
